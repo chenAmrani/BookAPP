547a9be0c459bd3abe51943244a96cebd2d6c4c5
orifarkash <oriturhvnkl@gmail.com>
2024-01-09 00:06:58 +0200

    ori commmit

--- src/routes/user_route.ts
+++ src/routes/user_route.ts
@@ -2,11 +2,7 @@ import express from "express";
 import UserController from '../controllers/user_controller';
 import authMiddleware from '../common/auth_middleware';
 import verifyOwenershipMiddleware from '../common/veifyOwenership_midleeware';
-<<<<<<< HEAD
-
-=======
 import adminMiddleware from '../common/admin_middleware';
->>>>>>> 3d23a1aae057c5212f8f42ede82819eae9d5c187
 
 const router = express.Router();
 //Admin
@@ -16,23 +12,15 @@ router.put("/update",authMiddleware,adminMiddleware,UserController.updateUser);
 router.delete("/delete/:id",authMiddleware,adminMiddleware,UserController.deleteUser);
 
 //author
-<<<<<<< HEAD
-router.get("/books",authMiddleware,authMiddleware,UserController.getMyBooks);
-=======
 router.get("/books",authMiddleware,UserController.getMyBooks);
 router.delete("/:id",authMiddleware,verifyOwenershipMiddleware,UserController.deleteUser);
->>>>>>> 3d23a1aae057c5212f8f42ede82819eae9d5c187
 
 
 //updating the user profile by himself.
-<<<<<<< HEAD
-router.put('/updateOwnProfile', authMiddleware,verifyOwenershipMiddleware,UserController.updateOwnProfile);
-=======
 router.put("/updateOwnProfile", authMiddleware,verifyOwenershipMiddleware,UserController.updateOwnProfile);
 
 
 
->>>>>>> 3d23a1aae057c5212f8f42ede82819eae9d5c187
 //get user by email
 router.get('/:email',authMiddleware,UserController.getUserByEmail);
 
