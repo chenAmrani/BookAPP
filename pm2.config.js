a6069cfed88c5fd7151fec0fea7fe07cd824d3c0
chenAmrani <chenamrani5@gmail.com>
2024-03-05 09:22:53 +0200

    scripts

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [{
         name: 'book-app',
         script: 'dist/src/server.js',
-        env_production: {
+        production: {
             NODE_ENV: 'production',
             dotenv_config_path: './.env.prod'
         }
